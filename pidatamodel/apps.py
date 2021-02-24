c8ccb056ff449063130d5122737db883738c6e14
Juuso Marttinen <juuso-dev@juusomarttinen.fi>
2021-02-24 17:53:54 +0200

    Created an app containing all database models
    Added a simple Portfolio table.

---
+++ pidatamodel/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PidatamodelConfig(AppConfig):
+    name = 'pidatamodel'
---
+++ pidatamodel/models.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.db import models
+
+
+class Portfolio(models.Model):
+    portfolio_ID = models.AutoField(primary_key=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
